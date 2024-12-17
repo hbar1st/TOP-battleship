59251f110470d2256fddce49e2c7df892fd39570
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-12-17 12:55:40 -0500

    feat: add form validation to landing page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,57 @@ import "./styles.css";
 import { createHumanPlayer, createComputerPlayer } from "./player.js";
 
 //create the main human player
-const playerYou = createHumanPlayer("You");
 
-const chooseHumanRB = document.querySelector("");
+const landingDialog = document.querySelector("#landing-dialog");
+const form = document.querySelector("form");
+const mainPlayerName = document.querySelector("#main-player");
+const opponentName = document.querySelector("#opp-name");
+
+mainPlayerName.addEventListener("input", () => {
+  if (mainPlayerName.value != "") {
+    //shutoff the error popup if user typed something in
+    mainPlayerName.setCustomValidity("");
+  }
+});
+
+opponentName.addEventListener("input", () => {
+  if (opponentName.value != "") {
+    //shutoff the error popup if user typed something in
+    opponentName.setCustomValidity("");
+  }
+});
+
+const submitBtn = document.querySelector("button");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  //validate the form before commencing the game
+  let valid = true;
+  if (mainPlayerName.value === "") {
+    mainPlayerName.setCustomValidity("Type in your name");
+    mainPlayerName.reportValidity();
+    valid = false;
+  } else {
+    mainPlayerName.setCustomValidity("");
+  }
+  const opponentChoices = form.elements.namedItem("opponent");
+  //check which opponent was selected and if form is valid
+  let checkedId = 0;
+  opponentChoices.forEach((el) => (el.checked ? (checkedId = el.id) : false));
+  console.log(checkedId);
+  if (checkedId === "the-computer") {
+  } else {
+    //check name given
+    const opponentName = document.querySelector("#opp-name");
+    if (opponentName.value === "") {
+      opponentName.setCustomValidity("Type in a name");
+      opponentName.reportValidity();
+      valid = false;
+    } else {
+      opponentName.setCustomValidity("");
+    }
+  }
+  if (valid) {
+    //commence the game
+    landingDialog.close();
+  }
+});
